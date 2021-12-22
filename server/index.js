c9b1abc27d517973f504d6c6b9e9a08225f1f273
Mathis <mathis.pati@gmail.com>
2021-12-23 00:28:28 +0100

    route to login to spotify with app credentials

--- server/index.js
+++ server/index.js
@@ -1,6 +1,28 @@
 require('dotenv').config();
+const cors = require('cors');
+const SpotifyWebApi = require('spotify-web-api-node');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+});
+
+app.use(cors());
+
+app.get('/api/login', async (req, res, next) => {
+    spotifyApi.clientCredentialsGrant()
+        .then(data => {
+            res.status(200).json({
+                accessToken: data.body.access_token,
+                expiresIn: data.body.expires_in
+            });
+        })
+        .catch(err => {
+            res.status(500).json({errorMessage: 'Une erreur est survenue lors de la récupération du token.'})
+        })
+});
 
 app.listen(PORT, () => console.log(`server launch on port ${PORT}`))
